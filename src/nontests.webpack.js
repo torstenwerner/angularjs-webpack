32125ac53b857f844bd648f4190f380ca74bde7c
Torsten Werner <torsten.werner@westernacher.com>
2017-09-25 07:40:49 +0200

    bugfix

--- src/nontests.webpack.js
+++ src/nontests.webpack.js
@@ -1,5 +1,3 @@
 // This file is an entry point for angular non test files.
 
-const context = require.context('./app', true, /(?!\.spec)\.js$/);
-
-context.keys().forEach(context);
+require('./app/app.js');
